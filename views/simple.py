edf3da7c6736719d664490249b733e68028fa5a1
Caleb Bartlett <callum@voxelfox.co.uk>
2020-05-02 09:54:10 +0100

    Fix topics to be more modular

--- views/simple.py
+++ views/simple.py
@@ -1,22 +1,50 @@
+import re as regex
 import toml
 
-from aiohttp.web import RouteTableDef, Request
+from aiohttp.web import RouteTableDef, Request, HTTPFound
 from aiohttp_jinja2 import template
 
 
 routes = RouteTableDef()
+url_matcher = regex.compile(r'\[(.+?)\]\((.+?) \"(.+?)\"\)')
+italics_matcher = regex.compile(r'_(.+)_')
+
+
+def get_projects_page(filename:str) -> dict:
+    """Returns a nice ol dict that can be passed to projects.j2"""
+
+    try:
+        with open(f"projects/{filename}.toml") as a:
+            data = toml.load(a)
+    except Exception:
+        if filename == 'index1':
+            data = {'project': []}
+        else:
+            return HTTPFound(location="/")
+    unchangeddata = [i for i in data['project'] if i['name'] != 'example']
+    data = []
+    for i in unchangeddata:
+        i['description'] = italics_matcher.sub(lambda m: f'<i>{m.group(1)}</i>', i['description'])
+        i['description'] = url_matcher.sub(lambda m: f'<a href="{m.group(2)}" alt="{m.group(3)}" target="_blank">{m.group(1)}</a>', i['description'])
+        data.append(i)
+    return {
+        'projects': data
+    }
 
 
 @routes.get("/")
-@template("index.j2")
+@template("projects.j2")
 async def index(request:Request):
     """Index page for the website"""
 
-    try:
-        with open("projects.toml") as a:
-            data = toml.load(a)
-    except Exception:
-        data = {'project': []}
-    return {
-        'projects': [i for i in data['project'] if i['name'] != 'example']
-    }
+    return get_projects_page('index')
+
+
+@routes.get("/{project_group_name}")
+@template("projects.j2")
+async def project_group(request:Request):
+    """Projects page"""
+
+    name = request.match_info['project_group_name']
+    return get_projects_page(name)
+
--- webserver.py
+++ webserver.py
@@ -1,18 +1,37 @@
+import asyncio
 import os
 
-from aiohttp.web import Application, run_app
+from aiohttp.web import Application, AppRunner, TCPSite
 from aiohttp_jinja2 import setup as jinja_setup
 from jinja2 import FileSystemLoader
 
 from views.simple import routes as simple_routes
 
-
 # Add routes and setups
-app = Application()
+app = Application(loop=asyncio.get_event_loop())
 app['static_root_url'] = '/static'
 jinja_setup(app, loader=FileSystemLoader(os.getcwd() + '/static/templates'))
 app.router.add_static('/static', os.getcwd() + '/static', append_version=True)
 app.router.add_routes(simple_routes)
 
-# Run app
-run_app(app)
+if __name__ == '__main__':
+    """Starts the bot (and webserver if specified) and runs forever"""
+
+    loop = app.loop
+
+    # HTTP server
+    application = AppRunner(app)
+    loop.run_until_complete(application.setup())
+    webserver = TCPSite(application, port=5002)
+
+    # Start server
+    loop.run_until_complete(webserver.start())
+
+    # This is the forever loop
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # Clean up our shit
+    loop.run_until_complete(application.cleanup())
