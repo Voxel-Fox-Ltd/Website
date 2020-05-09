0f960aa28f0800d4096d5f34e93b4adea4e34102
Caleb Bartlett <callum@voxelfox.co.uk>
2020-05-09 04:15:56 +0100

    Update project template to include support for images

--- views/frontend.py
+++ views/frontend.py
@@ -6,6 +6,9 @@ from aiohttp_jinja2 import template
 
 
 routes = RouteTableDef()
+
+
+image_matcher = regex.compile(r'!\[(.+?)?\]\((.+?)\)')
 url_matcher = regex.compile(r'\[(.+?)\]\((.+?) \"(.+?)\"\)')
 italics_matcher = regex.compile(r'_(.+)_')
 
@@ -25,7 +28,9 @@ def get_projects_page(filename:str) -> dict:
     data = []
     for i in unchangeddata:
         i['description'] = italics_matcher.sub(lambda m: f'<i>{m.group(1)}</i>', i['description'])
+        i['description'] = image_matcher.sub(lambda m: f'<img src="{m.group(2)}" />', i['description'])
         i['description'] = url_matcher.sub(lambda m: f'<a href="{m.group(2)}" alt="{m.group(3)}" target="_blank">{m.group(1)}</a>', i['description'])
+        i['description'] = i['description'].strip()
         data.append(i)
     return {
         'projects': data
