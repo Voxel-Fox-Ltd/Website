d35f0f60c1d43f393af29cc5a4a8e0d6c2fdfba6
Kae Bartlett <kae@voxelfox.co.uk>
2021-06-07 02:20:12 +0100

    Add cancel subscription page

--- website/paypal.py
+++ website/paypal.py
@@ -8,6 +8,8 @@ import aiohttp
 from aiohttp.web import Request, RouteTableDef, Response, json_response
 import pytz
 
+from .utils.get_paypal_access_token import get_paypal_access_token
+
 
 routes = RouteTableDef()
 PAYPAL_BASE = "https://api-m.paypal.com"  # "https://api-m.sandbox.paypal.com"
@@ -69,30 +71,6 @@ subscr_signup - Subscription signup (doesn't mean payment received)
 
 virtual_terminal - Payment received via virtual terminal
 """
-
-
-async def get_paypal_access_token(request: Request) -> str:
-    """
-    Get the PayPal access token from your client ID and secret.
-    """
-
-    # See if the token is expired
-    if PAYPAL_TOKEN_CACHE.get("expires_at", dt(2000, 1, 1)) > dt.utcnow():
-        return PAYPAL_TOKEN_CACHE.get("access_token")
-
-    # Grab a new token from PayPal
-    async with aiohttp.ClientSession() as session:
-        url = PAYPAL_BASE + "/v1/oauth2/token"
-        data = {"grant_type": "client_credentials"}
-        auth = aiohttp.BasicAuth(
-            request.app['config']['paypal_client_id'],
-            request.app['config']['paypal_client_secret'],
-        )
-        async with session.post(url, data=data, auth=auth) as r:
-            response = await r.json()
-    PAYPAL_TOKEN_CACHE.update(response)
-    PAYPAL_TOKEN_CACHE.update({"expires_at": dt.utcnow() + timedelta(seconds=response['expires_in'])})
-    return response['access_token']
 
 
 def get_expected_signature(transmission_id, timestamp, webhook_id, event_body):
---
+++ website/utils/get_paypal_access_token.py
@@ -0,0 +1,22 @@
+async def get_paypal_access_token(request: Request) -> str:
+    """
+    Get the PayPal access token from your client ID and secret.
+    """
+
+    # See if the token is expired
+    if PAYPAL_TOKEN_CACHE.get("expires_at", dt(2000, 1, 1)) > dt.utcnow():
+        return PAYPAL_TOKEN_CACHE.get("access_token")
+
+    # Grab a new token from PayPal
+    async with aiohttp.ClientSession() as session:
+        url = PAYPAL_BASE + "/v1/oauth2/token"
+        data = {"grant_type": "client_credentials"}
+        auth = aiohttp.BasicAuth(
+            request.app['config']['paypal_client_id'],
+            request.app['config']['paypal_client_secret'],
+        )
+        async with session.post(url, data=data, auth=auth) as r:
+            response = await r.json()
+    PAYPAL_TOKEN_CACHE.update(response)
+    PAYPAL_TOKEN_CACHE.update({"expires_at": dt.utcnow() + timedelta(seconds=response['expires_in'])})
+    return response['access_token']
